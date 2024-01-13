3d5e001d7566b12cb07f3458e9e7e88555150ebd
YaIsrak <61416887+YaIsrak@users.noreply.github.com>
2024-01-13 21:30:38 +0600

    ok

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -134,7 +134,7 @@ export default function Navigation() {
 				ref={sideBarRef}
 				className={cn(
 					'group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]',
-					isResetting && 'transition-all ease-in-out duration-300',
+					isResetting && 'h transition-all ease-in-out duration-300',
 					isMobile && 'w-0'
 				)}
 			>
